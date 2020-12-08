e659ca2a03dfb5c342f5dc27b5f56cf657e044e1
ramonavacarasu <vacarasuramona@gmail.com>
2020-12-08 12:38:20 +0200

    Stage 1

---
+++ Code Sharing Platform/task/src/platform/entity/Code.java
@@ -0,0 +1,21 @@
+package platform.entity;
+
+public class Code {
+
+    String code;
+
+    public Code() {
+    }
+
+    public Code(String code) {
+        this.code = code;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
---
+++ Code Sharing Platform/task/src/platform/repository/ICodeRepository.java
@@ -0,0 +1,9 @@
+package platform.repository;
+
+import platform.entity.Code;
+
+public interface ICodeRepository {
+
+    String getCode();
+
+}
